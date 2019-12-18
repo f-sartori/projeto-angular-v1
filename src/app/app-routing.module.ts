30b338ccb4218401ca9236e6a6499d4c091cfe7e
f-sartori <fmarcelo75@gmail.com>
2019-12-18 16:30:35 -0300

    projeto criado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './login/login.component';
+import { ContatoComponent } from './contato/contato.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+
+const routes: Routes = [
+  {path: 'home' , component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'registrar', component: RegistrarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContatoComponent } from './contato/contato.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutUsComponent,
+    ContatoComponent,
+    RegistrarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
